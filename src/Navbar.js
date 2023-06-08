e2390fbc194cd914a672f69bce9100cbabcdc92e
Daren Shamoun <darenshamoun@gmail.com>
2023-06-07 17:56:28 -0700

    Added some company logos and changed Navigation

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import styles from './Navbar.css';
+import styles from './Navbar.module.css';
+const logo = '/4k-LogoTransparent.png';
 
 function Navbar() {
   return (
     <nav className={styles.navbar}>
       <img src={logo} alt="Logo" className={styles.logo} />
       <ul className={styles.navLinks}>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/chat">Chat</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/services">Services</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
+        <li><Link to="/" className={styles.link}>Home</Link></li>
+        <li><Link to="/chat" className={styles.link}>Chat</Link></li>
+        <li><Link to="/about" className={styles.link}>About</Link></li>
+        <li><Link to="/services" className={styles.link}>Services</Link></li>
+        <li><Link to="/contact" className={styles.link}>Contact</Link></li>
       </ul>
     </nav>
   );
