b9c8981281c5680f00d2fe4e88915f575c8a183a
Daren Shamoun <darenshamoun@gmail.com>
2023-06-17 19:55:54 -0700

    updated chatInterface

--- src/chatInterface.js
+++ src/chatInterface.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import chatInterfaceStyles from './chatInterface.module.css';
 
 console.log(process.env.REACT_APP_SERVER_URL);
 
 function ChatInterface({ selectedParts }) {
   const [messages, setMessages] = useState([]);
+  const [aiIsTyping, setAiIsTyping] = useState(false); // New state for AI typing
 
   // This function retrieves the current context
   const getCurrentContext = () => {
@@ -19,7 +20,7 @@ function ChatInterface({ selectedParts }) {
     const input = event.target.elements.input.value;
   
     // Add the user's message to the chat history
-    setMessages([...messages, { text: input, sender: 'user' }]);
+    setMessages([...messages, { text: input, sender: 'user', timestamp: new Date().toLocaleTimeString() }]);
   
     // Prepare the messages to send to the server
     const chatMessages = [
@@ -37,11 +38,13 @@ function ChatInterface({ selectedParts }) {
     const context = getCurrentContext();
   
     // Send the messages, context, and selected parts to the server and get the AI's response
+    setAiIsTyping(true); // AI starts typing
     const response = await fetch(process.env.REACT_APP_SERVER_URL, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ messages: chatMessages, context: context, selectedParts: selectedParts })
     });    
+    setAiIsTyping(false); // AI finishes typing
   
     const data = await response.json();
     if (response.status === 500) {
@@ -50,24 +53,32 @@ function ChatInterface({ selectedParts }) {
     const aiMessage = data.message;
   
     // Add the AI's message to the chat history
-    setMessages(prevMessages => [...prevMessages, { text: aiMessage, sender: 'ai' }]);
+    setMessages(prevMessages => [...prevMessages, { text: aiMessage, sender: 'ai', timestamp: new Date().toLocaleTimeString() }]);
   
     // Clear the input field
     event.target.elements.input.value = '';
   };
-  
 
-  
+  // Scroll to the bottom of the chat when a new message is added
+  useEffect(() => {
+    const messagesDiv = document.querySelector(`.${chatInterfaceStyles.messages}`);
+    messagesDiv.scrollTop = messagesDiv.scrollHeight;
+  }, [messages]);
 
   return (
     <div className={chatInterfaceStyles.chatInterface}>
       <div className={chatInterfaceStyles.messages}>
-        {messages.map((message, index) => (
-          <div key={index} className={message.sender === 'user' ? chatInterfaceStyles.userMessage : chatInterfaceStyles.aiMessage}>
+      {messages.map((message, index) => (
+        <div key={index} style={{width: '100%', display: 'flex', justifyContent: message.sender === 'user' ? 'flex-end' : 'flex-start'}}>
+          <div className={message.sender === 'user' ? chatInterfaceStyles.userMessage : chatInterfaceStyles.aiMessage}>
             <strong>{message.sender.toUpperCase()}: </strong>
             <p>{message.text}</p>
+            <span className={chatInterfaceStyles.timestamp}>{message.timestamp}</span>
           </div>
-        ))}
+        </div>
+      ))}
+        {/* Add this line */}
+        {aiIsTyping && <div className={chatInterfaceStyles.aiTyping}>AI is typing...</div>}
       </div>
       <form onSubmit={handleInputSubmit} className={chatInterfaceStyles.inputForm}>
         <input name="input" type="text" className={chatInterfaceStyles.inputField} />
@@ -75,6 +86,7 @@ function ChatInterface({ selectedParts }) {
       </form>
     </div>
   );
+
 }
 
 export default ChatInterface;
