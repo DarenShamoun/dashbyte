27e9fe39c22efca44e3bf3a0836b7c0889ab61fd
Daren Shamoun <darenshamoun@gmail.com>
2023-06-17 20:26:25 -0700

    some changes to pcBuilder and its functionality

--- src/pcBuilder.js
+++ src/pcBuilder.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import styles from './pcBuilder.module.css'; // Import the styles
 
 function PcBuilder() {
   const [cpu, setCpu] = useState([]);
@@ -11,17 +12,26 @@ function PcBuilder() {
   const [selectedParts, setSelectedParts] = useState([]);
 
   useEffect(() => {
-    const fetchParts = async (partType, setPartState) => {
+    const fetchParts = async () => {
       try {
-        const response = await axios.get(`/api/parts/${partType}`);
-        console.log(`Fetched ${partType}:`, response.data); // Add this line
-        setPartState(response.data);
-      } catch (error) {
-        console.error(`Failed to fetch ${partType}: ${error}`);
+        const cpu = await axios.get('http://localhost:5000/api/parts/cpu');
+        setCpu(cpu.data);
+        const gpu = await axios.get('http://localhost:5000/api/parts/gpu');
+        setGpu(gpu.data);
+        const ram = await axios.get('http://localhost:5000/api/parts/ram');
+        setRam(ram.data);
+        const ssd = await axios.get('http://localhost:5000/api/parts/ssd');
+        setSsd(ssd.data);
+        const hdd = await axios.get('http://localhost:5000/api/parts/hdd');
+        setHdd(hdd.data);
+        const usb = await axios.get('http://localhost:5000/api/parts/usb');
+        setUsb(usb.data);
+      } catch (err) {
+        console.error(`Failed to fetch parts: ${err}`);
       }
     };
     
-
+    
     fetchParts('cpu', setCpu);
     fetchParts('gpu', setGpu);
     fetchParts('ram', setRam);
@@ -35,10 +45,10 @@ function PcBuilder() {
   };
 
   return (
-    <div>
+    <div className={styles.pcBuilder}>
       <h1>PC Builder Page</h1>
       <p>Select parts to build your PC:</p>
-      <div>
+      <div className={styles.partSelection}>
         <h2>CPU:</h2>
         <select onChange={(e) => onPartSelect(JSON.parse(e.target.value))}>
           {cpu.map(part => (
@@ -48,7 +58,7 @@ function PcBuilder() {
           ))}
         </select>
       </div>
-      <div>
+      <div className={styles.partSelection}>
         <h2>GPU:</h2>
         <select onChange={(e) => onPartSelect(JSON.parse(e.target.value))}>
           {gpu.map(part => (
@@ -58,7 +68,7 @@ function PcBuilder() {
           ))}
         </select>
       </div>
-      <div>
+      <div className={styles.partSelection}>
         <h2>RAM:</h2>
         <select onChange={(e) => onPartSelect(JSON.parse(e.target.value))}>
           {ram.map(part => (
@@ -68,7 +78,7 @@ function PcBuilder() {
           ))}
         </select>
       </div>
-      <div>
+      <div className={styles.partSelection}>
         <h2>SSD:</h2>
         <select onChange={(e) => onPartSelect(JSON.parse(e.target.value))}>
           {ssd.map(part => (
@@ -78,17 +88,17 @@ function PcBuilder() {
           ))}
         </select>
       </div>
-      <div>
+      <div className={styles.partSelection}>
         <h2>HDD:</h2>
         <select onChange={(e) => onPartSelect(JSON.parse(e.target.value))}>
           {hdd.map(part => (
             <option key={part._id} value={JSON.stringify(part)}>
-              {part.Brand} {part.Model} - ${part.Price}
+                            {part.Brand} {part.Model} - ${part.Price}
             </option>
           ))}
         </select>
       </div>
-      <div>
+      <div className={styles.partSelection}>
         <h2>USB:</h2>
         <select onChange={(e) => onPartSelect(JSON.parse(e.target.value))}>
           {usb.map(part => (
