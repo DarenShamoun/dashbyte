7ef72f20157bf114bcf6ff41acf1364eb4bcb2ce
Daren Shamoun <darenshamoun@gmail.com>
2023-06-19 13:19:05 -0700

    added shutdown modularization as well

--- Server/server.js
+++ Server/server.js
@@ -5,9 +5,9 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const { connectToMongoDB, db, client } = require('./db');
-
 const partsRoute = require('./routes/parts');
 const chatRoute = require('./routes/chat');
+const shutdown = require('./shutdown');
 
 const app = express();
 app.use(bodyParser.json());
@@ -29,12 +29,4 @@ chatRoute(app);
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 
-// Close the MongoDB connection when the server shuts down
-process.on('SIGINT', async () => {
-  console.log('Server is shutting down...');
-  await client.close();
-  server.close(() => {
-    console.log('Server has shut down');
-    process.exit(0);
-  });
-});
+shutdown(server, client);
