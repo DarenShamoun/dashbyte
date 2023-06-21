2923ca9de37f9d142b35ad602fbf6d6163ef347e
Daren Shamoun <darenshamoun@gmail.com>
2023-06-20 21:52:33 -0700

    updated some important features

--- src/components/Chat/chatFunctions.js
+++ src/components/Chat/chatFunctions.js
@@ -1,7 +1,12 @@
 import { v4 as uuidv4 } from 'uuid';
-import axios from 'axios';
 
-export const handleInputSubmit = async (e, input, setInput, messages, setMessages, setAiIsTyping) => {
+export const getCurrentContext = (selectedParts) => {
+  // Convert the selectedParts array to a string
+  return JSON.stringify(selectedParts);
+};
+
+
+export const handleInputSubmit = async (e, input, setInput, messages, setMessages, selectedParts, setAiIsTyping) => {
   e.preventDefault();
   if (input.trim() !== '') {
     const newMessage = {
@@ -13,13 +18,39 @@ export const handleInputSubmit = async (e, input, setInput, messages, setMessage
     setInput('');
     setAiIsTyping(true);
 
-    // Send the user's message to your server for processing
-    const response = await axios.post('/api/chat', { message: input });
+    // Prepare the messages to send to the server
+    const chatMessages = [
+      {
+        role: 'system',
+        content: 'You are a helpful IT assistant.'
+      },
+      {
+        role: 'user',
+        content: input
+      }
+    ];
 
-    // After receiving the response from the server, create a new message object for the AI's response
+    // Get the current context (this will depend on your application)
+    const context = getCurrentContext(selectedParts);
+
+    // Send the user's message to your server for processing
+    const response = await fetch(process.env.REACT_APP_SERVER_URL, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ messages: chatMessages, context: context, selectedParts: selectedParts })
+    });
+
+    // Check for server error
+    if (response.status === 500) {
+      const data = await response.json();
+      console.error('Server error:', data.error);
+      return;
+    }
+
+    const data = await response.json();
     const aiMessage = {
       id: uuidv4(),
-      text: response.data.message, // Use the message from the server's response
+      text: data.message, // Use the message from the server's response
       sender: 'ai',
     };
 
